a4cffeafc22964a239dbdf7c4e986e2ef7b86425
Erika Carolina Quintero Molina <kika.quintemo@gmail.com>
2023-11-26 07:23:07 -0500

    se suben modificaciones del index haciendo conexiones con los botones, añadiendo el formulario de creación de carpeta y redireccionamiento de esta creada

--- js/js.js
+++ js/js.js
@@ -1,109 +1,76 @@
-    const tarjeta1 = document.getElementById('tarjeta1')
-    const tarjeta2 = document.getElementById('tarjeta2')
-    const tarjeta3 = document.getElementById('tarjeta3')
-    const modulo = document.getElementById('modulo')
-    const botonesmodulo = document.getElementById('botonesmodulo')
-    const botonbuscar = document.getElementById('botonbuscar') 
-    const bienvenida =document.getElementById ('bienvenida')
-    const logo =document.getElementById ('logo')
-    const botonguardar = document.getElementById('botonguardar')
-    const botonanalisis = document.getElementById('botonanalisis')
-    const seccionbiblioteca = document.getElementById('seccionbiblioteca')
+    const seccionbiblioteca = document.getElementById('seccionbiblioteca');
+    const savebutton = document.getElementById('save');
+    const abrirPopUp = document.getElementById('CrearNewFolder');
+    const cerrarPopUp = document.getElementById('CancelButton');
+    const PopUp = document.getElementById('Form1');
+    const formulario = document.querySelector('.Formulario');
+    const abrirConfirmacionCarpeta = document.getElementById('AcceptButton');
+    const cerrarConfirmacionCarpeta = document.getElementById('CloseButton');
+    const ConfirmacionCrearCarpeta = document.getElementById('ConfirmacionCrearCarpeta');
     
-    
-    function setearActivoAlHacerClic (elementoABuscar, idElemento){
-        elementoABuscar.addEventListener('click', function(){
-            console.log('hago clic en mi función')
-            botonClickeado = true; 
-            if(botonClickeado) {
-                bienvenida.hidden = true; 
-                logo.hidden = true; 
-                tarjeta1.hidden = true;
-                tarjeta2.hidden = true;
-                tarjeta3.hidden = true;
-                botonesmodulo.innerHTML= `<section id="botonesmodulo">
-                <div class="botonesmodulo" id="botonesmodulo">
-                <div class="buttonssa">
-                <button class="button" id="search" aria-label="Botón de Búsqueda" >Buscar</button>
-                </div>
-                <div class="buttonssa" id="buttonlibrary">
-                <button class="button" id="save" aria-label="Botón de Biblioteca" >Biblioteca</button>
-                </div>
-                <div class="buttonssa">
-                <button class="button" id="analysis" aria-label="Botón de Análisis" >Camino al aprendizaje</button>
-                </div>
-                </div>
-                </section>`;
-                // seccionbiblioteca.hidden=true;
+                abrirPopUp.addEventListener("click",()=>{
+                    console.log('Abriendo formulario');
+                    PopUp.showModal();
+                    formulario.reset();
+
+                }) 
+                cerrarPopUp.addEventListener("click",()=>{
+                    console.log('Cerrando formulario');
+                    PopUp.close();
+                    formulario.reset();
+                       
+                })
+
+                formulario.addEventListener('submit', function(event){
+                    event.preventDefault();
+
                 
-                const buttons = document.querySelectorAll('.button')
-                const elementoASeleccionar = document.getElementById(idElemento)
-                 
-                elementoASeleccionar.addEventListener('click', function(){
-                buttons.forEach((btn) => {
-                    btn.classList.remove('activo');
+                    if(validarFormulario()){
+                        console.log('Formulario válido. Enviar formulario...');
+                        ConfirmacionCrearCarpeta.style.display = 'block';
+                        ConfirmacionCrearCarpeta.showModal();
+                                               
+                    } else{
+                        console.log('Formulario no válido. Verifica los campos.');
+                    }
+                   
                 });
-                this.classList.add('activo');
+                function validarFormulario(){
+                    const NombreProyecto = document.getElementById('NombreProyecto');
+                    const Objetivo = document.getElementById('Objetivo');
+
+                    if(NombreProyecto.value.trim()===''){
+                        alert('El campo Nombre del Proyecto no puede estar vacío.');
+                        return false;
+                    }
+
+                    if(Objetivo.value.trim()===''){
+                        alert('El campo Objetivo no puede estar vacío.');
+                        return false;
+                    }
+
+                    return true
+                }          
+
+                    cerrarConfirmacionCarpeta.addEventListener("click",()=>{
+                        ConfirmacionCrearCarpeta.close();
+                        PopUp.close();
+                        ConfirmacionCrearCarpeta.style.display = 'none';
+                    }) 
+                     
             
-                console.log('Le diste click al botón '+ idElemento)
-            });
-
-            const searchbutton = document.getElementById('search')
-    
-                        searchbutton.addEventListener('click', function(){
-                            buttons.forEach((btn) => {
-                                btn.classList.remove('activo');
-                            });
-                            this.classList.add('activo');
-                            // seccionbiblioteca.hidden=true;
                 
-                            console.log('Le diste click al botón buscar')
-                        });
-             
-            const savebutton = document.getElementById('save');
-
-            savebutton.addEventListener('click', function(){
-                buttons.forEach((btn) => {
-                    btn.classList.remove('activo');
-                });
-                this.classList.add('activo');
-            //     seccionbiblioteca.innerHTML= `<section id="seccionbiblioteca">
-            //     <div class="bannerseccionbiblioteca">
-            //         <div class="bienvenida" id="bienvenida">
-            //             <img id="saves" src="./images/save.svg" alt="">
-            //             <h3>Bienvenido a tu Biblioteca de Exploración</h3>
-            //             <p>Aqui podras encontrar y compartir la información que guardaste para crear tus proyectos</p>
-            //         </div>
-            //         <img id="logo" src="images/perrito_profesor.svg" alt="Avatar EdVisto" width="242px" height="367px">
-            //     </div>
-            // </section>`
-            // seccionbiblioteca.hidden=false;
                 
                 console.log('Le diste click al botón biblioteca')
-            });
-            
-            const analysisbutton = document.getElementById('analysis');
 
-            analysisbutton.addEventListener('click', function(){
-                buttons.forEach((btn) => {
-                    btn.classList.remove('activo');
-                });
-                this.classList.add('activo');
-                // seccionbiblioteca.hidden=true;
-                
-                console.log('Le diste click al botón análisis')
-            });
-            elementoASeleccionar.click();
+   
+                       
             
-            }  
-           
             
-        console.log('Le diste click al botón buscar')
-                    
-    });
-    }
-        let botonClickeado = false; 
-        setearActivoAlHacerClic(botonbuscar, "search")
-        setearActivoAlHacerClic(botonguardar, "save")
-        setearActivoAlHacerClic(botonanalisis,"analysis")
+
+
+   
+
+       
+
      
